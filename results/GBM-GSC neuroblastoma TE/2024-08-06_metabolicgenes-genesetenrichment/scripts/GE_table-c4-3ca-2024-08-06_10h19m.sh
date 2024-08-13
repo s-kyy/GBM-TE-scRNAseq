086e9481fe68908c7f18fcfb814b9e30ca98f565
s-kyy <samantha.y.twentyfourteen@gmail.com>
2024-08-13 11:29:37 -0400

    adding gsea metabolic gene scripts + stdout results

---
+++ results/GBM-GSC neuroblastoma TE/2024-08-06_metabolicgenes-genesetenrichment/scripts/GE_table-c4-3ca-2024-08-06_10h19m.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#SBATCH --time=00:30:00
+#SBATCH --account=def-ytanaka
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --array=0-11
+#SBATCH --mail-user=samantha.y.twentyfourteen@gmail.com
+#SBATCH --mail-type=ALL
+if [[ $SLURM_ARRAY_TASK_ID == 0 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster0_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster0_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 1 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster1_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster1_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 2 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster2_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster2_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 3 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster3_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster3_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 4 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster4_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster4_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 5 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster5_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster5_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 6 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster6_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster6_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 7 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster7_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster7_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 8 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster8_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster8_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 9 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster9_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster9_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 10 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster10_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster10_c4-3ca_GE_table 500 5 160 
+fi
+
+if [[ $SLURM_ARRAY_TASK_ID == 11 ]] ; then
+/home/samkyy/scratch/gsea/run_gsea4.sh /home/samkyy/scratch/gsea/GE_table.gct /home/samkyy/scratch/gsea/phenotypes_ge.cls#cluster11_versus_REST /home/samkyy/scratch/gsea/genesets/c4.3ca.v2023.2.Hs.symbols.gmt gene_set cluster11_c4-3ca_GE_table 500 5 160 
+fi
+
